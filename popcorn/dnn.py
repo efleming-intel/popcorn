4615549d3cefda86218bbd2476b1a48f62053277
Orel Yehuda <orel.yehuda@intel.com>
2024-01-23 16:33:39 -0800

    [WIP] Removed Pandas - TODO: use Case to keep track of labels

--- popcorn/dnn.py
+++ popcorn/dnn.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 class dnn_log:
     def __init_(self):
         self.filename = ''
@@ -17,18 +14,6 @@ class dnn_log:
 
         return data
 
-    def load_log_dnnl_timestamp(self):
-        # dnnl_verbose,629411020589.218018,exec,cpu,convolution,jit:avx2,forward_inference,src_f32::blocked:abcd:f0 wei_f32::blocked:Acdb8a:f0 bia_f32::blocked:a:f0 dst_f32::blocked:aBcd8b:f0,,alg:convolution_direct,mb1_ic3oc96_ih227oh55kh11sh4dh0ph0_iw227ow55kw11sw4dw0pw0,1.21704
-        data = pd.read_csv(self.filename, names=[ 'dnnl_verbose','timestamp','exec','arch','type', 'jit', 'pass', 'fmt', 'opt', 'alg', 'shape', 'time', 'dummy'], engine='python')
-        
-        return data
-
-    def load_log_dnnl_timestamp_backend(self):
-        #"onednn_verbose,operation,engine,primitive,implementation,prop_kind,memory_descriptors,attributes,auxiliary,problem_desc"
-        # dnnl_verbose,629411020589.218018,primitive,exec,cpu,convolution,jit:avx2,forward_inference,src_f32::blocked:abcd:f0 wei_f32::blocked:Acdb8a:f0 bia_f32::blocked:a:f0 dst_f32::blocked:aBcd8b:f0,,alg:convolution_direct,mb1_ic3oc96_ih227oh55kh11sh4dh0ph0_iw227ow55kw11sw4dw0pw0,1.21704
-        data = pd.read_csv(self.filename, names=[ 'dnnl_verbose','timestamp','backend','exec','arch','type', 'jit', 'pass', 'fmt', 'opt', 'alg', 'shape', 'time', 'dummy'], engine='python')
-        
-        return data
     
 
     def load_dnnl_log(self):
